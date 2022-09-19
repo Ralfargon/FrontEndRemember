4b61c1979a401b829d908038f14864fbfd9b2d50
Rafael Carvalho <rafasgcarvalho@gmail.com>
2022-09-19 16:25:36 -0300

    First Commit

---
+++ src/components/Comment.jsx
@@ -0,0 +1,37 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import { Avatar } from './Avatar';
+import styles from './Comment.module.css';
+
+export function Comment() {
+    return (
+        <div className={styles.comment}>
+            <Avatar
+                hasBorder={false}
+                src="https://github.com/Ralfargon.png"
+                alt=""
+            />
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Rafael C</strong>
+                            <time title="11 de Maio às 08:00h" dateTime="2022-05-11 08:00:00">Cerca de uma 1h atrás</time>
+                        </div>
+
+                        <button title="Deletar comentário">
+                            <Trash size={24} />
+                        </button>
+                    </header>
+                    <p>Muito bom! parabéns!</p>
+                </div>
+                <footer>
+                    <button>
+                        <ThumbsUp />
+                        Aplaudir <span>20</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    )
+}
